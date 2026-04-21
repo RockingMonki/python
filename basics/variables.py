bdc040c52b9543cbf3fb3070a0d45fed1b6cf37b
RockingMonki <vgsr65@gmail.com>
2026-04-21 16:07:35 +0530

    Initial Commit

---
+++ basics/variables.py
@@ -0,0 +1,10 @@
+# Variables are a way to store data. Meaning storing something in the memory.
+
+name = "Vivek"  # here name is storing the string "Vivek"
+
+# can call the variable name to access the value
+print(name)  # prints it
+
+# can change the value of variable
+name = "changed name"
+print(name)
